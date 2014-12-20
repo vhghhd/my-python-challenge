700697fdf9f72c295d39fd4393c5b553dc18dbaf
zhangye <vhghhd0222@126.com>
2014-12-20 15:10:21 +0800

    forth
    done it

--- forth/forth.py
+++ forth/forth.py
@@ -13,11 +13,28 @@ def findforth(path):
     return path+'/'+''.join(msg)+'.html'
 
 def jumptorealanswer(html):
-    htmlfront = html[:html.rfind('/')]
-    print htmlfront
-    print urllib.urlopen(html).read()
+    htmlfront = html[:html.rfind('/')+1]
+    data = urllib.urlopen(html).read()
+    num = filter(str.isdigit,data)
+    print num
+    if num and int(num) < 100000000:
+        jumptorealanswer(htmlfront+'/linkedlist.php?nothing='+num)
+    else:
+        print data
 
 if __name__=="__main__":
     path = "http://www.pythonchallenge.com/pc/def/0.html"
     frontpath = path[:path.rfind('/')]
     jumptorealanswer(findforth(frontpath))
+    # hit1: linkedlist.html
+    jumptorealanswer(frontpath+'/linkedlist.php')
+    # hit2: linkedlist.php?nothing=12345
+    jumptorealanswer(frontpath+'/linkedlist.php?nothing=12345')
+    # hit3: Yes. 16044 Divide by two and keep going.
+    jumptorealanswer(frontpath+'/linkedlist.php?nothing='+str(16044/2))
+    # hit4: There maybe misleading numbers in the 
+    #       text. One example is 82683. Look only
+    #       for the next nothing and the next nothing is 63579
+    jumptorealanswer(frontpath+'/linkedlist.php?nothing='+str(63579))
+    # http://www.pythonchallenge.com/pc/def/peak.html
+    
